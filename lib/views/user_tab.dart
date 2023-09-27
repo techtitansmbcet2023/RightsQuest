a2731078bd08997206fcd5cff9023540fce79e7e
Shivsay <shivsa543@gmail.com>
2023-09-27 12:56:03 +0530

    User tab updates

--- lib/views/user_tab.dart
+++ lib/views/user_tab.dart
@@ -5,6 +5,33 @@ class UserTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Padding(
+      padding: EdgeInsets.all(8),
+
+      child: ListView(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(color: Colors.deepOrange,borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    Icon(Icons.person_2),
+
+                    SizedBox(width: 30,),
+
+                    Flexible(child: Text("Hello name ",softWrap: true,)),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
