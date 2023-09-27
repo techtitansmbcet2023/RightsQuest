e1e951f8095c7630d43ffbc15701aad48c8ee9ee
Shivsay <shivsa543@gmail.com>
2023-09-27 18:58:36 +0530

    quiz page updates

--- lib/views/quiz.dart
+++ lib/views/quiz.dart
@@ -13,6 +13,16 @@ class QuizPage extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: ListView(
           children: [
+            Text(
+              "Chapter 3: Cyber Bullying", // Title
+              style: TextStyle(
+                fontSize: 24, // Adjust the font size as needed
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
             Text("Mira had been excited about her new online gaming community. She made friends from around the world and enjoyed playing games with them. However, one day, things took an unexpected turn. She started receiving hurtful messages and threats from an unknown user within the gaming community.\nAt first, Mira didn't want to worry her friends, so she kept it a secret. But one day, Rohan noticed the change in her behavior. Mira did not seem like her cheerful and energetic self. Rohan was worried about Mira and decided to ask if something happened.",style: TextStyle(
               fontSize: 18,
             ),),
