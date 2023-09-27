9f83ab4e389f0835dfce2ef5eb2148b09e0d6bb7
Shivsay <shivsa543@gmail.com>
2023-09-27 15:37:14 +0530

    Updated quiz intro page

--- lib/views/quiz_intro.dart
+++ lib/views/quiz_intro.dart
@@ -12,13 +12,9 @@ class QuizPage extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: ListView(
           children: [
+            Image(image: AssetImage('lib/assets/images/sketch_3_clipdrop-cleanup.jpeg')),
+            
             Text("When selecting a name, consider the target audience and the app's overall mission. It's important to choose a name that is easy to remember, spell, and conveys a sense of empowerment and advocacy for children's rights. Additionally, check for trademark availability and domain name availability if you plan to have a website for your app.",softWrap: true,),
-
-            Container(
-              width: double.infinity,
-              height: 200,
-              color: Colors.lime,
-            )
           ],
         ),
       ),
